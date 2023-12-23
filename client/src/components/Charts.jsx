1c524b1a1b62195c43298fa6fa42b97bc4a1a5fd
Nadia <nadiahosisy@gmail.com>
2023-12-23 13:02:23 +0200

    added charts

---
+++ client/src/components/Charts.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { LineChart } from "@mui/x-charts/LineChart";
+
+// export const Charts = () => {
+//   return (
+//     <LineChart
+//       width={500}
+//       height={300}
+//       series={[{ data: uData, label: "uv", area: true, showMark: false }]}
+//       xAxis={[{ scaleType: "point", data: xLabels }]}
+//       sx={{
+//         ".MuiLineElement-root": {
+//           display: "none",
+//         },
+//       }}
+//     />
+//   );
+// };
