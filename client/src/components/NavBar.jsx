16bbc18b1adaae17736871566b23a3ceecf84ab4
Nadia <nadiahosisy@gmail.com>
2024-01-04 21:37:15 +0200

    added logout function

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import {
   AppBar,
   Toolbar,
   IconButton,
   Typography,
-  Stack,
   Button,
-  Menu,
-  MenuItem,
+  Stack,
 } from "@mui/material";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import ShowChartRoundedIcon from "@mui/icons-material/ShowChartRounded";
 import { useAuth } from "../context/AuthProvider";
 
 export const NavBar = () => {
-  const { userData } = useAuth();
-  const [anchorNav, setAnchorNav] = useState(null);
-  const openMenu = (event) => setAnchorNav(event.currentTarget);
-  const closeMenu = () => setAnchorNav(null);
+  const { userData, logout } = useAuth();
+  const navigate = useNavigate();
 
-  const pages = userData
-    ? ["Home", "About", "My Stocks"]
-    : ["Home", "About", "My Stocks", "Login"];
-  const pagePaths = userData
-    ? ["/", "/about", "/mystocks"]
-    : ["/", "/about", "/mystocks", "/login"];
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+  const capitalizeFirstLetter = (string) => {
+    if (!string) return "";
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const pages = ["Home", "About", "My Stocks"];
+  const pagePaths = ["/", "/about", "/mystocks"];
 
   return (
     <AppBar
@@ -54,61 +55,41 @@ export const NavBar = () => {
           spacing={2}
           sx={{ display: { xs: "none", md: "flex" } }}
         >
+          {userData && (
+            <Typography
+              sx={{
+                display: "flex",
+                flexGrow: 0,
+                fontSize: "22px",
+                textAlign: "center",
+                alignItems: "center",
+                alignContent: "center",
+                justifyContent: "center",
+                color: "yellow",
+                fontFamily: "Normal",
+              }}
+            >
+              Hello, {capitalizeFirstLetter(userData.name)}
+            </Typography>
+          )}
           {pages.map((page, index) => (
             <Button
               key={page}
               color="inherit"
               component={NavLink}
               to={pagePaths[index]}
-              style={({ isActive }) => ({
-                backgroundColor: isActive ? "rgba(40, 45, 71, 0.5)" : "inherit",
-                color: isActive ? "#ffffff" : "inherit",
-              })}
             >
               {page}
             </Button>
           ))}
-          {userData && (
-            <Typography
-              sx={{
-                flexGrow: 0,
-                textAlign: "center",
-                alignItems: "center",
-                color: "yellow",
-              }}
-            >
-              Hello, {userData.name}
-            </Typography>
-          )}
+
+          <Button onClick={handleLogout} size="large" style={{ color: "red" }}>
+            {userData ? "Logout" : "Login"}
+          </Button>
         </Stack>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ display: { xs: "flex", md: "none" } }}
-          onClick={openMenu}
-        >
-          <MenuRoundedIcon />
-        </IconButton>
-        <Menu
-          anchorEl={anchorNav}
-          open={Boolean(anchorNav)}
-          onClose={closeMenu}
-          sx={{ display: { xs: "flex", md: "none" } }}
-        >
-          {pages.map((page, index) => (
-            <MenuItem
-              key={page}
-              onClick={closeMenu}
-              component={NavLink}
-              to={pagePaths[index]}
-            >
-              {page}
-            </MenuItem>
-          ))}
-        </Menu>
       </Toolbar>
     </AppBar>
   );
 };
+
+export default NavBar;
--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -1,20 +1,36 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(() => {
+    const storedUserData = localStorage.getItem("userData");
+    return storedUserData ? JSON.parse(storedUserData) : null;
+  });
+
+  async function logout() {
+    try {
+      setUserData(null);
+      window.localStorage.removeItem("userData");
+    } catch (error) {
+      console.error("Error during logout:", error);
+      throw error;
+    }
+  }
 
   const updateUserData = (newData) => {
     setUserData(newData);
-    console.log(newData, "its mt context");
-    console.log(userData);
+    if (newData) {
+      localStorage.setItem("userData", JSON.stringify(newData));
+    } else {
+      localStorage.removeItem("userData");
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ userData, updateUserData }}>
+    <AuthContext.Provider value={{ userData, updateUserData, logout }}>
       {children}
     </AuthContext.Provider>
   );
