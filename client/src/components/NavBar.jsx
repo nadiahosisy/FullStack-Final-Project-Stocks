a5fea67966c2bd792e6e6a4506d7b32883651fbe
nadiahosisy <nadiahosisy@gmail.com>
2024-01-27 12:12:45 +0200

    fix responsive home page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,11 +6,10 @@ import {
   IconButton,
   Typography,
   Button,
-  Stack,
   Avatar,
   Popover,
 } from "@mui/material";
-
+import MenuIcon from "@mui/icons-material/Menu";
 import ShowChartRoundedIcon from "@mui/icons-material/ShowChartRounded";
 import { useAuth } from "../context/AuthProvider";
 
@@ -18,6 +17,7 @@ export const NavBar = () => {
   const { userData, logout, isLoggedIn } = useAuth();
   const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const handleLogout = () => {
     logout();
@@ -30,6 +30,10 @@ export const NavBar = () => {
 
   const handlePopoverClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleMobileMenuToggle = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
   };
 
   const open = Boolean(anchorEl);
@@ -49,69 +53,31 @@ export const NavBar = () => {
       position="static"
     >
       <Toolbar style={{ minHeight: "60px" }}>
+        {/* Show the menu icon only on xs (extra-small) screens */}
         <IconButton
           size="large"
           edge="start"
-          aria-label="logo"
-          sx={{
-            display: { xs: "none", md: "flex" },
-            color: "#3fbdb4",
-          }}
-        >
-          <ShowChartRoundedIcon />
-        </IconButton>
+          color="inherit"
+          aria-label="menu"
+          sx={{ display: { xs: "block", md: "none" }, mr: 1 }}
+          onClick={handleMobileMenuToggle}
+        ></IconButton>
         <Typography
           variant="h6"
           component="div"
           sx={{
             flexGrow: 1,
-            display: { xs: "none", md: "flex", fontFamily: "Normal" },
+            display: { xs: "none", md: "block" }, // Only display on md screens and larger
+            fontFamily: "Normal",
+            ml: { xs: 0, md: 2 },
           }}
         >
           Stock Prediction App
         </Typography>
-        <Stack
-          direction="row"
-          spacing={2}
-          sx={{ display: { xs: "none", md: "flex" } }}
-          alignItems="center"
+        <div
+          className="nav-links"
+          sx={{ display: { xs: "none", md: "flex" }, alignItems: "center" }} // Only display on md screens and larger
         >
-          {userData && (
-            <>
-              <Avatar
-                alt={capitalizeFirstLetter(userData.name)}
-                src={userData.imageUrl}
-                sx={{
-                  width: 40,
-                  height: 40,
-                  marginRight: 1,
-                  cursor: "pointer",
-                }}
-                onClick={handleAvatarClick}
-              />
-              <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handlePopoverClose}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-              >
-                <Typography
-                  sx={{
-                    p: 2,
-                    color: "black",
-                    backgroundColor: "#fff7f6",
-                    fontFamily: "Normal",
-                  }}
-                >
-                  Hello, {capitalizeFirstLetter(userData.name)} 👋
-                </Typography>
-              </Popover>
-            </>
-          )}
           {pages.map((page, index) => (
             <Button
               key={page}
@@ -130,7 +96,44 @@ export const NavBar = () => {
           >
             {isLoggedIn ? "Logout" : "Login"}
           </Button>
-        </Stack>
+        </div>
+        {userData && (
+          <>
+            <Avatar
+              alt={capitalizeFirstLetter(userData.name)}
+              src={userData.imageUrl}
+              sx={{
+                width: 40,
+                height: 40,
+
+                cursor: "pointer",
+                display: { xs: "none", md: "flex" }, // Show only in desktop view
+              }}
+              onClick={handleAvatarClick}
+            />
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handlePopoverClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+            >
+              <Typography
+                sx={{
+                  p: 2,
+                  color: "black",
+                  backgroundColor: "#fff7f6",
+                  fontFamily: "Normal",
+                }}
+              >
+                Hello, {capitalizeFirstLetter(userData.name)} 👋
+              </Typography>
+            </Popover>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
